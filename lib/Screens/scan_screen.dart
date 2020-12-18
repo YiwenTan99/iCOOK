d503332ea3443bedbd42622453a7e5924b506e8e
YiwenTan99 <yumikotan1999@gmail.com>
2020-12-18 15:48:24 +0800

    1st combine

--- lib/Screens/scan_screen.dart
+++ lib/Screens/scan_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite/tflite.dart';
 
 class ScanScreen extends StatefulWidget {
   @override
@@ -7,6 +10,22 @@ class ScanScreen extends StatefulWidget {
 }
 
 class _ScanScreenState extends State<ScanScreen> {
+  List _outputs;
+  File _image;
+  bool _loading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loading = true;
+
+    loadModel().then((value) {
+      setState(() {
+        _loading = false;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,9 +42,132 @@ class _ScanScreenState extends State<ScanScreen> {
         automaticallyImplyLeading: false,
         centerTitle: true,
       ),
-      body: new Center(
-        child: new Text("Scan page")
+      body: _loading
+          ? Container(
+        alignment: Alignment.center,
+        child: CircularProgressIndicator(),
       )
+          : Container(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _image == null ? Container() : Image.file(_image),
+            SizedBox(
+              height: 20,
+            ),
+            _outputs != null
+                ? Text(
+              "${_outputs[0]["label"]}",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20.0,
+                background: Paint()..color = Colors.white,
+              ),
+            )
+                : Container()
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton( onPressed: (){
+        _showChoiceDialog(context);
+      },
+        child: Icon(Icons.image),
+      ),
+
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: pickImage,
+      //   child: Icon(Icons.image),
+      // ),
     );
+  }
+
+   Future<void> _showChoiceDialog(BuildContext context){
+    return showDialog(context: context,builder: (BuildContext context){
+      return AlertDialog(
+        title: Text("Load Image From"),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              GestureDetector(
+                child: Text("Gallery"),
+                onTap: () {
+                  _openGallary(context);
+                },
+              ),
+              Padding(padding: EdgeInsets.all(8.0)),
+              GestureDetector(
+                child: Text("Camera"),
+                onTap: () {
+                  _openCamera(context);
+                },
+              ),
+            ],
+          ),
+        ),
+      );
+    });
+  }
+
+  _openGallary(context) async {
+    // ignore: deprecated_member_use
+    var picture = await ImagePicker.pickImage(source: ImageSource.gallery);
+    this.setState(() {
+      _loading = true;
+      _image = picture;
+    });
+    Navigator.of(context).pop();
+    classifyImage(_image);
+  }
+
+  _openCamera(BuildContext context) async{
+    WidgetsFlutterBinding.ensureInitialized();
+    // ignore: deprecated_member_use
+    var picture = await ImagePicker.pickImage(source: ImageSource.camera);
+    this.setState(() {
+      _loading = true;
+      _image = picture;
+    });
+    Navigator.of(context).pop();
+    classifyImage(_image);
+  }
+
+  pickImage() async {
+    // ignore: deprecated_member_use
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    if (image == null) return null;
+    setState(() {
+      _loading = true;
+      _image = image;
+    });
+    classifyImage(image);
+  }
+
+  classifyImage(File image) async {
+    var output = await Tflite.runModelOnImage(
+      path: image.path,
+      numResults: 15,
+      threshold: 0.9,
+      imageMean: 127.5,
+      imageStd: 127.5,
+    );
+    setState(() {
+      _loading = false;
+      _outputs = output;
+    });
+  }
+
+  loadModel() async {
+    await Tflite.loadModel(
+      model: "assets/Test/model.tflite",
+      labels: "assets/Test/labels.txt",
+    );
+  }
+
+  @override
+  void dispose() {
+    Tflite.close();
+    super.dispose();
   }
 }
